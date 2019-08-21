8bc1bf0fa1b9b73e468c42c2669d9f3353a4ef1c
turningpoint1988 <35712510+turningpoint1988@users.noreply.github.com>
2019-08-21 21:59:53 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@
 for eachTF in `ls ./pbmdata/`
 do 
 	echo $eachTF
+	# 'model_shallow_noshape' should be modified according to '-run' and '-model' commands, e.g. 'model_' + 'shallow' + '_' + 'noshape'
 	if [ -d ./model_shallow_noshape/$eachTF ]; then
 	   echo $eachTF 'has existed.'
 	   continue
