89e76e8bc3273c567010c73864212130e0cf8eb3
turningpoint1988 <35712510+turningpoint1988@users.noreply.github.com>
2019-08-21 21:53:56 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@
 for eachTF in `ls ./pbmdata/`
 do 
 	echo $eachTF
-	if [ -d ./model_MGW/$eachTF ]; then
+	if [ -d ./model_shallow_noshape/$eachTF ]; then
 	   echo $eachTF 'has existed.'
 	   continue
 	fi
     # run: 'MGW', 'ProT', 'Roll', 'HelT', 'MPRH'
-    python train_val_test_hybrid.py -datadir ./pbmdata/$eachTF/data -run 'MGW' -batchsize 300 -k 5 -params 30 --train
+    python train_val_test.py -datadir <data path> -run 'noshape' -model 'shallow' -batchsize 300 -k 5 -params 30 --train
 done
 
