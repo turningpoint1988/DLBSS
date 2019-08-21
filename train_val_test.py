9aa422a0fc9458781eaa00142664309004159a77
turningpoint1988 <35712510+turningpoint1988@users.noreply.github.com>
2019-08-21 21:35:56 +0800

    Update train_val_test.py

--- train_val_test.py
+++ train_val_test.py
@@ -6,7 +6,7 @@ from os.path import join, abspath, dirname, exists
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.optimizers import Adadelta
 from keras.models import load_model
-from models import DeepCNN, DeepBind
+from models import DeepCNN, DeepBind_K
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
@@ -15,20 +15,21 @@ from utils import *
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Convert sequence and target")
-    parser.add_argument('-datadir', dest='datadir', type=str, help='Positive data for training, testing')
+    parser.add_argument('-datadir', dest='datadir', type=str, help='PBM data for training, testing')
     parser.add_argument('-k', dest='k_folds', type=int, default=5, help='k-folds cross-validation')
     parser.add_argument('-batchsize', dest='batchsize', type=int, default=300, help='the size of one batch')
     parser.add_argument('-ratio', dest='ratio', type=float, default=0.2, help='the propotion of validation data over the training data')
     parser.add_argument('-params', dest='params', type=int, default=1, help='the number of paramter settings')
     parser.add_argument('--train', dest='train', action='store_true', help='train and test step')
     parser.add_argument('--no-train', dest='train', action='store_false', help='only test step.')
-    parser.add_argument('-plot', dest='plot', action='store_true', default=True, help='only test step')
-    parser.add_argument('-run', dest='run', type=str, default='onehot', help='three encoding methods, including onehot, kmer, gappedkmer')
-    
+    parser.add_argument('-plot', dest='plot', action='store_true', default=True, help='plot the training process')
+    parser.add_argument('-run', dest='run', type=str, default='onehot', help='onehot')
+    parser.add_argument('-model', dest='model', type=str, default='shallow', help='two models, including shallow and deep')
+
     return parser.parse_args()
 
-def main():
 
+def main():
     file_path = dirname(abspath(__file__))
     args = parse_args()
     
@@ -58,10 +59,11 @@ def main():
 
     R2 = []
     
-    if not exists(file_path + '/model/%s' % name):
-       print 'Building' + file_path + '/model/%s' % name
-       os.mkdir(file_path + '/model/%s' % name)
-    f_params = open(file_path + '/model/%s/params.txt' % name, 'w')
+    model_name = 'model_' + args.run
+    if not exists(file_path + '/%s/%s' % (model_name, name)):
+       print 'Building ' + file_path + '/%s/%s' % (model_name, name)
+       os.makedirs(file_path + '/%s/%s' % (model_name, name))
+    f_params = open(file_path + '/%s/%s/params.txt' % (model_name, name), 'w')
     for fold in range(args.k_folds):        
         x_train = seqs_data_train[train_ids[fold]]
         y_train = intensity_train[train_ids[fold]]
@@ -79,10 +81,16 @@ def main():
                print >> f_params, "the {}-th paramter setting of the {}-th folds is {}".format(params_num, fold, params)
                
                print 'Building model...'
-               model = DeepBind(input_shape, params)
+               if args.model == 'deep':
+                  model = DeepCNN(input_shape, params)
+               elif args.model == 'shallow':
+                  model = DeepBind_K(input_shape, params)
+               else:
+                   print >> sys.stderr, 'invalid command!'; sys.exit(1)
                #return
-               checkpointer = ModelCheckpoint(filepath=file_path + '/model/%s/params%d_bestmodel_%dfold.hdf5' 
-                                                % (name, params_num, fold), monitor='val_loss', verbose=1, save_best_only=True)
+               checkpointer = ModelCheckpoint(filepath=file_path + '/%s/%s/params%d_bestmodel_%dfold.hdf5' 
+                                                % (model_name, name, params_num, fold), 
+                                                monitor='val_loss', verbose=1, save_best_only=True)
                earlystopper = EarlyStopping(monitor='val_loss', patience=15, verbose=1)
 
                print 'Training model...'
@@ -91,20 +99,21 @@ def main():
                History = model.fit(x_train, y_train, epochs=100, batch_size=args.batchsize, shuffle=True,
                                    validation_data=(x_valid, y_valid), callbacks=[checkpointer, earlystopper], verbose=2)
                history_all[str(params_num)] = History
-           best_num = SelectBest(history_all, file_path + '/model/%s/' % name, fold, 'val_loss')
+           best_num = SelectBest(history_all, file_path + '/%s/%s/' % (model_name, name), fold, 'val_loss')
            if args.plot:
-              PlotandSave(history_all[str(best_num)], file_path + '/model/%s/figure_%dfold.png' % (name, fold), fold, 'val_loss')
+              PlotandSave(history_all[str(best_num)], 
+              file_path + '/%s/%s/figure_%dfold.png' % (model_name, name, fold), fold, 'val_loss')
         print >> f_params, "\n\n" 
         f_params.flush() 
         print 'Testing model...'
         # load_model('')
-        model.load_weights(file_path + '/model/%s/params%d_bestmodel_%dfold.hdf5' % (name, best_num, fold))
+        model.load_weights(file_path + '/%s/%s/params%d_bestmodel_%dfold.hdf5' % (model_name, name, best_num, fold))
         results = model.evaluate(x_test, y_test)
         print results
         y_pred = model.predict(x_test, batch_size=args.batchsize, verbose=1)
         y_pred = np.asarray([y[0] for y in y_pred])
         y_real = np.asarray([y[0] for y in y_test])
-        with open(file_path + '/model/%s/score_%dfold.txt' % (name, fold), 'w') as f:
+        with open(file_path + '/%s/%s/score_%dfold.txt' % (model_name, name, fold), 'w') as f:
            assert len(y_pred) == len(y_real), 'dismathed!'
            for i in range(len(y_pred)):
                print >> f, '{:.4f} {}'.format(y_pred[i], y_real[i])
@@ -115,16 +124,10 @@ def main():
        
     f_params.close()
     print "the mean R2 is {}.".format(np.mean(R2))
-    outfile = file_path + '/model/%s/metrics.txt' % (name)
+    outfile = file_path + '/%s/%s/metrics.txt' % (model_name, name)
     with open(outfile,'w') as f:
         for i in range(len(R2)):
             print >> f, "{:.4f}".format(R2[i])
         print >> f, "{:.4f}".format(np.mean(R2))
     
 if __name__ == '__main__': main()    
-    
-    
-    
-    
-    
-    
